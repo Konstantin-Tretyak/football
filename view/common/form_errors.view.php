6bf6a15ab96c6d296c73ee75dc82ef130107ccbf
Kostya <princ_grenlandii@ukr.net>
2016-06-11 18:20:37 +0300

    ~~

--- view/common/form_errors.view.php
+++ view/common/form_errors.view.php
@@ -2,7 +2,7 @@
     <div class="alert alert-danger">
         <ul class="list-unstyled">
             <?php foreach ($errors as $field => $error): ?>
-                <li><?php echo $field.':'.$error; ?></li>
+                <li><?php echo $field.':'.$error ?></li>
             <?php endforeach ?>
         </ul>
     </div>
